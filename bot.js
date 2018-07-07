b4a216b6f5e6ba6261436ba6f2d4176469089bf0
legend-boker <39490532+legend-boker@users.noreply.github.com>
2018-07-07 21:52:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,6 +70,47 @@ BT.editChannel("459306277487771651", {name : "• WELCOME TO KND •🌹"})
 
 });
 
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Kinga Never Die")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'chat');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Kinga Never Dieا")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(`وِلكےـمِےـ نٌےـوِرتُےـ ٱلسًےـيّےـرفُےـر يّےــٱ حًےـلوِ  ${member} `) 
